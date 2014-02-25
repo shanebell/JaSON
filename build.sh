e591e58a79298174141ad165981dece367f1454f
Shane Bell <shane.bell@gmail.com>
2014-02-26 08:46:48 +1100

    Shane: added content type of "None" which sets an empty blank content type header.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#   Copyright 2013 Shane Bell
+#   Copyright 2014 Shane Bell
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
