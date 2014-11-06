dc2c88e2bb9dc8791cc890e9041be0336b1718ce
Shane Bell <shane.bell@gmail.com>
2014-11-06 22:32:43 +1100

    Shane: WIP

--- app/scripts/controllers/HistoryController.js
+++ app/scripts/controllers/HistoryController.js
@@ -8,10 +8,22 @@ angular.module('JaSON')
 			function init() {
 				$log.debug('Initialising HistoryController...');
 
+				bindVars();
 				bindFunctions();
 			}
 
+			function bindVars() {
+				$scope.model = {
+					history: []
+				};
+			}
+
 			function bindFunctions() {
+
+				$scope.clearHistory = function() {
+					$log.debug('Clearing history');
+					$scope.model.history = [];
+				};
 
 			}
 
