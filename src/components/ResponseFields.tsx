9ad429a2b12e153969250f0f52ab928357c7fec5
Shane Bell <shane.bell@gmail.com>
2020-04-16 22:55:01 +1000

    Cleanup of AceEditor related code. Made headers use AceEditor for consistency Minor tweaks to colour theme to reduce colours because it was too much with the colours from Ace.

--- src/components/ResponseFields.tsx
+++ src/components/ResponseFields.tsx
@@ -4,30 +4,16 @@ import { makeStyles } from "@material-ui/core/styles";
 import Tab from "@material-ui/core/Tab";
 import Tabs from "@material-ui/core/Tabs";
 import React from "react";
-import ResponseHeaders from "./ResponseHeaders";
 import StatusCode from "./StatusCode";
 import TabPanel from "./TabPanel";
 import useApplicationState from "../state";
 import ResponseTime from "./ResponseTime";
-import AceComponent from "./AceComponent";
+import ResponseData from "./ResponseData";
+import ResponseHeaders from "./ResponseHeaders";
 
 const useStyles = makeStyles((theme) => ({
   responseTabs: {
     marginBottom: theme.spacing(4),
-  },
-  response: {
-    padding: theme.spacing(2),
-  },
-  rawResponse: {
-    padding: theme.spacing(2),
-  },
-  headers: {
-    padding: theme.spacing(2),
-  },
-  code: {
-    fontFamily: "'Source Code Pro', monospace",
-    fontSize: "16px",
-    whiteSpace: "pre-wrap",
   },
   chips: {
     position: "absolute",
@@ -65,21 +51,21 @@ const ResponseFields: React.FC = () => {
 
         {/* FORMATTED RESPONSE */}
         <TabPanel isActive={state.responseTab === 0 && state.response.data}>
-          <Paper className={classes.response} variant="outlined">
-            <AceComponent response={state.response} />
+          <Paper variant="outlined">
+            <ResponseData response={state.response} />
           </Paper>
         </TabPanel>
 
         {/* RAW RESPONSE */}
         <TabPanel isActive={state.responseTab === 1 && state.response.data}>
-          <Paper className={classes.rawResponse} variant="outlined">
-            <AceComponent response={state.response} formatted={false} />
+          <Paper variant="outlined">
+            <ResponseData response={state.response} formatted={false} />
           </Paper>
         </TabPanel>
 
         {/* RESPONSE HEADERS */}
         <TabPanel isActive={state.responseTab === 2 && state.response.data}>
-          <Paper className={classes.headers} variant="outlined">
+          <Paper variant="outlined">
             <ResponseHeaders headers={state.response.headers} />
           </Paper>
         </TabPanel>
