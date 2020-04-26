6ef0da21282a1475344234a67007b4e0e13ebf55
Shane Bell <shane.bell@gmail.com>
2020-04-26 13:28:53 +1000

    material-ui version update updated styling of history list

--- src/components/HistoryList.tsx
+++ src/components/HistoryList.tsx
@@ -5,6 +5,7 @@ import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import Chip from "@material-ui/core/Chip";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import { makeStyles } from "@material-ui/core/styles";
@@ -30,16 +31,11 @@ const useStyles = makeStyles((theme) => ({
     minWidth: "48px",
   },
   method: {
-    display: "inline-block",
-    fontSize: "12px",
-    backgroundColor: theme.palette.grey[300],
-    color: theme.palette.grey[800],
-    padding: `${theme.spacing(1)}px ${theme.spacing(3)}px`,
-    margin: `${theme.spacing(1)}px 0`,
-    borderRadius: theme.spacing(4),
+    margin: `${theme.spacing(2)}px 0`,
   },
-  inline: {
-    display: "inline",
+  date: {
+    display: "inline !important",
+    marginLeft: theme.spacing(2),
   },
   trim: {
     display: "block",
@@ -130,21 +126,25 @@ const HistoryList: React.FC = () => {
                 <ListItemText
                   primary={<div className={classes.trim}>{historyItem.path}</div>}
                   secondary={
-                    <React.Fragment>
+                    <>
                       <Typography component="span" variant="body2" className={classes.trim}>
                         {historyItem.domain}
                       </Typography>
-                      <span className={classes.method}>{historyItem.method}</span>
-                      {false && (
-                        <Typography component="span" variant="body2" className={classes.method}>
-                          {historyItem.method}
-                        </Typography>
-                      )}
-                      <Typography component="span" variant="caption" className={classes.trim}>
+                      <Chip
+                        className={classes.method}
+                        label={historyItem.method}
+                        color="primary"
+                        size="small"
+                        variant="outlined"
+                      />
+                      <Typography component="span" variant="caption" className={`${classes.trim} ${classes.date}`}>
                         {historyItem.date}
                       </Typography>
-                    </React.Fragment>
+                    </>
                   }
+                  secondaryTypographyProps={{
+                    component: "span",
+                  }}
                 />
               </Tooltip>
               <ListItemSecondaryAction>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -24,6 +24,7 @@ const useStyles = makeStyles((theme) => ({
   logo: {
     height: "48px",
     marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(2),
   },
 }));
 
