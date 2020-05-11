228f15f8d51715c8cd29add0a71d04fe42c62ad1
Shane Bell <shane.bell@gmail.com>
2020-05-11 21:21:01 +1000

    Added icon attribution to about page.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,12 +4,14 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Slide from "@material-ui/core/Slide";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, Theme } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import React from "react";
 import config from "../config";
 import "typeface-roboto";
 import logo from "../images/jason.png";
+import { mdiBug, mdiGithub, mdiGoogleChrome, mdiLicense, mdiCreativeCommons } from "@mdi/js";
+import Icon from "@mdi/react";
 import { TransitionProps } from "@material-ui/core/transitions/transition";
 
 const useStyles = makeStyles((theme) => ({
@@ -36,12 +38,25 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const paragraphStyles = makeStyles((theme) => ({
+const paragraphStyles = makeStyles((theme: Theme) => ({
   paragraph: {
     padding: theme.spacing(1),
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
   link: {
-    color: theme.palette.primary.main,
+    color: theme.palette.text.secondary,
+  },
+  icon: {
+    color: theme.palette.text.secondary,
+    padding: theme.spacing(1),
+  },
+  image: {
+    width: "36px",
+    height: "36px",
+    padding: theme.spacing(1),
   },
 }));
 
@@ -52,19 +67,26 @@ const Transition = React.forwardRef(function Transition(
   return <Slide direction="down" ref={ref} {...props} />;
 });
 
-const Paragraph: React.FC<{ text?: string; href: string; target: string; linkText: string }> = ({
-  text,
-  href,
-  target,
-  linkText,
-}) => {
+const Paragraph: React.FC<{
+  text?: any;
+  href: string;
+  target: string;
+  linkText: string;
+  icon?: string;
+  image?: any;
+}> = ({ text, href, target, linkText, icon, image }) => {
   const classes = paragraphStyles();
   return (
     <Typography variant="body1" align="center" className={classes.paragraph}>
-      {text}{" "}
-      <a href={href} target={target} className={classes.link}>
-        {linkText}
-      </a>
+      {icon && <Icon path={icon} title={linkText} size={1.5} className={classes.icon} />}
+      {image && <img src={image} alt="JaSON logo" className={classes.image} />}
+      <div>
+        {text}
+        &nbsp;
+        <a href={href} target={target} className={classes.link}>
+          {linkText}
+        </a>
+      </div>
     </Typography>
   );
 };
@@ -96,33 +118,46 @@ const About: React.FC<{ open: boolean; onClose: React.ReactEventHandler }> = ({ 
             <img src={logo} alt="JaSON logo" className={classes.img} />
           </div>
           <Paragraph
-            text="Developed and maintained by"
-            href="mailto:shane.bell@gmail.com"
-            target="_mail"
-            linkText="Shane Bell"
+            icon={mdiGithub}
+            text="View the source code on"
+            href="https://github.com/shanebell/JaSON"
+            target="_github"
+            linkText="GitHub"
           />
           <Paragraph
-            text="Licensed under the"
-            href="http://www.apache.org/licenses/LICENSE-2.0"
-            target="_license"
-            linkText="Apache License v2.0"
-          />
-          <Paragraph
+            icon={mdiGoogleChrome}
             text="Available from the"
             href="https://chrome.google.com/webstore/detail/jason/oealdlhfjifhgbmjnenhkgffglaibojf"
             target="_webstore"
             linkText="Chrome Web Store"
           />
           <Paragraph
-            text="Source code available at"
-            href="https://github.com/shanebell/JaSON"
-            target="_github"
-            linkText="GitHub"
-          />
-          <Paragraph
+            icon={mdiBug}
             href="https://chrome.google.com/webstore/detail/jason/oealdlhfjifhgbmjnenhkgffglaibojf/support"
             target="_issue"
-            linkText="Report an issue"
+            text="Contact the developer to"
+            linkText="report an issue"
+          />
+          <Paragraph
+            icon={mdiLicense}
+            text="Licensed under the"
+            href="http://www.apache.org/licenses/LICENSE-2.0"
+            target="_license"
+            linkText="Apache License v2.0"
+          />
+          <Paragraph
+            image={logo}
+            text="Jason Voorhees icon by"
+            href="http://laurareen.com/"
+            target="_laurareen"
+            linkText="Laura Reen"
+          />
+          <Paragraph
+            icon={mdiCreativeCommons}
+            text="Icon licensed under the"
+            href="https://creativecommons.org/licenses/by/4.0/"
+            target="_creativecommons"
+            linkText="CC Attribution license"
           />
         </DialogContent>
       </DialogContent>
