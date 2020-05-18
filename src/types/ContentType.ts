1a2b5bca3764dc3909e5791fd95996e5eb0621d9
Shane Bell <shane.a.bell@iag.com.au>
2020-05-18 17:49:14 +1000

    Tidy up to pull content types and http methods into the type definitions.

--- src/types/ContentType.ts
+++ src/types/ContentType.ts
@@ -2,3 +2,25 @@ export default interface ContentType {
   name: string;
   value: string;
 }
+
+const APPLICATION_JSON: ContentType = {
+  name: "JSON (application/json)",
+  value: "application/json",
+};
+
+const TEXT_XML: ContentType = {
+  name: "XML (text/xml)",
+  value: "text/xml",
+};
+
+const APPLICATION_XML: ContentType = {
+  name: "XML (application/xml)",
+  value: "application/xml",
+};
+
+const MULTIPART_FORM_DATA: ContentType = {
+  name: "Form data (multipart/form-data)",
+  value: "multipart/form-data",
+};
+
+export { APPLICATION_JSON, TEXT_XML, APPLICATION_XML, MULTIPART_FORM_DATA };
